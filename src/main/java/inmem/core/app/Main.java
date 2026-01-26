f5e719afadc8041e0483a36ae92137c0fa4cefdb
Ghost <duchojakub987@gmail.com>
2026-01-26 17:23:48 +0100

    main repl loop

--- src/main/java/inmem/core/app/Main.java
+++ src/main/java/inmem/core/app/Main.java
@@ -1,7 +1,35 @@
 package inmem.core.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, world");
-  }
+
+    public static void main(String[] args) {
+        try {
+            repl();
+        }catch (IOException e) {
+            System.out.println("Error: " + e);
+            System.exit(-1);
+        }
+    }
+
+    public static void repl() throws IOException {
+        //clear screen
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.printf("inmem > ");
+            final String query = buff.readLine().trim();
+
+            if (query.equals("quit")) {
+                break;
+            }
+        }
+    }
+
 }
