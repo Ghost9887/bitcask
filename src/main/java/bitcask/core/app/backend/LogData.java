bd202b0709f9ec15cec4de1523b2c5d30ef8f943
Ghost <duchojakub987@gmail.com>
2026-01-29 20:40:47 +0100

    crc algo

--- src/main/java/bitcask/core/app/backend/LogData.java
+++ src/main/java/bitcask/core/app/backend/LogData.java
@@ -18,6 +18,40 @@ public final class LogData {
         this.key = key;
         this.value = value;
     }
+    
+    //TODO: refactor to make it work here
+    public static void calculateCRC(String bits, String divisor) {
+        //append the zeros;
+        StringBuilder str = new StringBuilder(bits);
+        str.append("0".repeat(divisor.length() - 1));
+        System.out.println(str);
+
+        //starting set of bits
+        StringBuilder res = new StringBuilder(bits.substring(0, divisor.length()));
+        int index = divisor.length() - 1;
+
+        while (index <= str.length()) {
+            StringBuilder temp = new StringBuilder();
+            if (res.charAt(0) == '0') {
+                temp.append(res.substring(1, res.length()));
+            }
+            else {
+                for (int i = 1; i < res.length(); i++) {
+                    if (res.charAt(i) == divisor.charAt(i)) {
+                        temp.append('0');
+                    }
+                    else {
+                        temp.append('1');
+                    }
+                }
+            }
+            res = temp;
+            index += 1;
+            if (index >= str.length()) break;
+            res.append(str.charAt(index));
+        }
+        System.out.println(res);
+    }
 
     public void convertToBinary() {
         StringBuilder str = new StringBuilder();
